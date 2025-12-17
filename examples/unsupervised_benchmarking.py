224f4e4f89d8b9c8bd4663f8abd9d5011353c9da
siemdejong <28396796+siemdejong@users.noreply.github.com>
2025-12-17 10:50:55 +0100

    uncomment other segmentation algorithms in example

--- examples/unsupervised_benchmarking.py
+++ examples/unsupervised_benchmarking.py
@@ -4,7 +4,7 @@ import segmenteer as seg
 
 
 def main():
-    path = Path("CMU-1-Small-Region.svs")
+    path = Path("TA232.svs")
     original_image = seg.load_image(
         path=path, level=1
     )
@@ -24,11 +24,11 @@ def main():
     print(f"Output directory: {output_dir}\n")
 
     segmenters = [
-        # seg.OtsuSegmenter(),
-        # seg.EntropyMaskerSegmenter(),
-        # seg.GrandQCSegmenter(confidence_threshold=0.5, min_area=10),
-        # seg.HESTSegmenter(mpp=1.0, confidence_threshold=0.5, min_area=10),
-        # seg.CPGSegmenter(docker_image="cpg-tissuemasker:latest", min_area=10),
+        seg.OtsuSegmenter(),
+        seg.EntropyMaskerSegmenter(),
+        seg.GrandQCSegmenter(confidence_threshold=0.5, min_area=10),
+        seg.HESTSegmenter(mpp=1.0, confidence_threshold=0.5, min_area=10),
+        seg.CPGSegmenter(docker_image="cpg-tissuemasker:latest", min_area=10),
         seg.FESISegmenter(),
         seg.FESISegmenter(improved=False),
     ]
